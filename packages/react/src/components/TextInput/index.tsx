baeb9a80a585533790a3d54090881d4492bd2864
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-25 23:21:15 -0300

    Refactor TextInput component to support forwarding refs

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,15 +1,19 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementRef, forwardRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  containerProps?: ComponentProps<typeof TextInputContainer>
 }
 
-export function TextInput({ prefix, ...props }: Readonly<TextInputProps>) {
-  return (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
-    </TextInputContainer>
-  )
-}
+// eslint-disable-next-line react/display-name
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, containerProps, ...props }, ref) => {
+    return (
+      <TextInputContainer {...containerProps}>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  },
+)
